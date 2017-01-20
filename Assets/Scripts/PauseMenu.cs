503d1a23cbeb89ee62cc94f61435064ee8162570
AIE\bradley.court <bradleycourt@hotmail.com>
2017-01-21 09:02:13 +1030

    Added pausemenu, added code in shockhandler to handle clicking whilst paused

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    //private bool isShowing;
+    bool isPaused = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape") && !isPaused)
+        {
+            pauseGame();
+        }
+        else if (Input.GetKeyDown("escape") && isPaused)
+        {
+            UnPause();
+        }
+    }
+    void pauseGame()
+    {
+        //pauseMenu.SetActive(isShowing);
+        pauseMenu.SetActive(true);
+
+            Time.timeScale = 0;
+            isPaused = true;
+            //isShowing = !isShowing;
+            //pauseMenu.SetActive(isShowing);
+
+    }
+
+    public void UnPause()
+    {
+        print("yo");
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+}
