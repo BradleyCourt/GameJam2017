9309ad1210760615d45c5af85452930bd0feadf1
AIE\bradley.court <bradleycourt@hotmail.com>
2017-01-21 01:19:08 +1030

    added level select start screen and timer, start screen is fixed with scaling resolution level select does not

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameManager.currentLevelData.timeLeft -= Time.deltaTime;
+        if (GameManager.currentLevelData.timeLeft <= 0)
+        {
+            // TO DO restart game
+        }
+	}
+}
