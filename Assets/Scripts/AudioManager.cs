3218f1cdea1a33f25c97fdb0d32b6f61cdc933d5
AIE\bradley.court <bradleycourt@hotmail.com>
2017-01-21 16:28:38 +1030

    Added sounds to main menu and gameplay, also added a star for collectables

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public List<AudioClip> AudioClips;
+
+    public AudioSource aSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        aSource = GetComponent<AudioSource>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (!aSource.isPlaying)
+        {
+            playRandomClip();
+        }
+	}
+
+    void playRandomClip()
+    {
+        if (AudioClips.Count > 1)
+        {
+            AudioClip temp = AudioClips[0];
+
+            int index = Random.Range(1, AudioClips.Count - 1);
+
+            AudioClips[0] = AudioClips[index];
+            AudioClips[index] = temp;
+
+            aSource.clip = AudioClips[0];
+            aSource.Play();
+        }
+        else if( AudioClips.Count == 1)
+        {
+            aSource.clip = AudioClips[0];
+            aSource.Play();
+        }
+    }
+
+}
