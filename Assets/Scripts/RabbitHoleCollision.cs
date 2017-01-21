0188b113d72237021e4deb199566f42532cb1a9f
AIE\bradley.court <bradleycourt@hotmail.com>
2017-01-21 13:36:32 +1030

    rabbit hole now kills you, update to eggsmash to support this

---
+++ Assets/Scripts/RabbitHoleCollision.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RabbitHoleCollision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        // Check if the collided object is tagged as "Player"
+        if (col.collider.tag == "Player")
+        {
+            EggSmash[] eggs = col.gameObject.GetComponents<EggSmash>();
+            Debug.Log(eggs.Length);
+            if (eggs.Length > 0)
+            {
+                foreach(EggSmash E in eggs)
+                {
+                    E.Smash();
+                }
+            }
+            // Find all instances of EggSmash script on the collided object
+            
+            // For each smash script, call the Smash() function.
+        }
+
+
+
+    }
+}
