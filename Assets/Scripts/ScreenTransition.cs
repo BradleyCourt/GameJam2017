f597fb872e8fb982ad406333e6d7a0e8e6577fd4
hql180 <huyql@hotmail.com>
2017-01-21 21:27:59 +1030

    Finally got screen transition working

---
+++ Assets/Scripts/ScreenTransition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenTransition : MonoBehaviour
+{
+    public Material TransitionMat;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+
+    }
+}
---
+++ Assets/Scripts/TransitionBlit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class TransitionBlit : MonoBehaviour
+{
+    public Material TransitionMat;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if(TransitionMat != null)
+        {
+            Graphics.Blit(src, dst, TransitionMat);
+        }
+    }
+}
