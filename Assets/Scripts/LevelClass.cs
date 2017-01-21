c3b1b321d03652a1c54f427e4415e447809bd044
AIE\jake.perry <jake-perry@live.com>
2017-01-21 15:56:07 +1030

    Removed old basket models and imported correct model. Set up prefab with new script to stop egg movement once they land in the basket. Began work on win condition checking. Locked easter egg movement on the z-axis. Added high score tracking for each level through the level class script.

--- Assets/Scripts/LevelClass.cs
+++ Assets/Scripts/LevelClass.cs
@@ -6,6 +6,12 @@ public class LevelClass {
 	public bool passed = false;
 	public string levelName = "";
 
+	// Track high scores
+	public int hsCollectibles = 0;
+	public int hsEggs = 0;
+	public int hsWavesRemaining = 0;
+	public float hsTime = 0.0f;
+
 	public LevelClass (bool pass, string name)		// Constructor
 	{
 		passed = pass;
@@ -15,6 +21,24 @@ public class LevelClass {
 	public LevelClass (string name)		// Constructor
 	{
 		levelName = name;
+	}
+
+	public void Complete (int collectibles, int eggs, int remainingWaves, float time)
+	{
+		passed = true;
+
+		// Set high scores if applicable
+		if (collectibles > hsCollectibles)
+			hsCollectibles = collectibles;
+
+		if (eggs > hsEggs)
+			hsEggs = eggs;
+
+		if (remainingWaves > hsWavesRemaining)
+			hsWavesRemaining = remainingWaves;
+
+		if ( (time < hsTime) || hsTime == 0.0f)
+			hsTime = time;
 	}
 
 }
